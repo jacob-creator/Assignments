b2349d0440e9a7608e374c6ce049651833f16505
jacob-creator <55944721+jacob-creator@users.noreply.github.com>
2020-12-30 08:37:03 -0600

    Assignment 3.1
    Homework for Nate

--- Driver.java
+++ Driver.java
@@ -1,30 +1,29 @@
 public class Driver {
 
     public static void main(String[] args) {
-        double gradeAvg = -20.0;
-        char gradeLetter;
-
-        if (gradeAvg < 0) {
-            System.out.println("Error! That is not a grade!");
-        } else if (gradeAvg >=0 && gradeAvg <= 59.9) {
-            System.out.println("Your grade average is " + gradeAvg + "%!\n" + "Your grade is an F!");
-        } else if (gradeAvg >= 60.0 && gradeAvg <= 69.9) {
-            System.out.println("Your grade average is " + gradeAvg + "%!\n" + "Your grade is an D!");
-        } else if (gradeAvg >=70 && gradeAvg <=75){
-            System.out.println("Your grade average is " + gradeAvg + "%!\n" + "Your grade is an C!");
-        } else if (gradeAvg >=75.1 && gradeAvg <=79.9){
-            System.out.println("Your grade average is " + gradeAvg + "%!\n" + "Your grade is an C+!");
-        } else if (gradeAvg >=80 && gradeAvg <=82.9) {
-            System.out.println("Your grade average is " + gradeAvg + "%!\n" + "Your grade is an B-!");
-        } else if (gradeAvg >=83 && gradeAvg <=85.9) {
-            System.out.println("Your grade average is " + gradeAvg + "%!\n" + "Your grade is an B!");
-        } else if (gradeAvg >=86 && gradeAvg <=89.9) {
-            System.out.println("Your grade average is " + gradeAvg + "%!\n" + "Your grade is an B+!");
-        } else if (gradeAvg >=90 && gradeAvg <=92.9) {
-            System.out.println("Your grade average is " + gradeAvg + "%!\n" + "Your grade is an A-!");
-        } else if (gradeAvg >=93 && gradeAvg <=100) {
-            System.out.println("Your grade average is " + gradeAvg + "%!\n" + "Your grade is an A!");
+        System.out.print("This is multiples of 3: \n");
+        int i = 0;
+        while (i < 1001) {
+            if (i % 3 == 0) {
+                System.out.print(i + " ");
+            }
+            i++;
+        }
+        System.out.print("\n\nThis is multiples of 5: \n");
+        int b = 0;
+        while (b < 1001) {
+            if (b % 5 == 0) {
+                System.out.print(b + " ");
+            }
+            b++;
+        }
+        System.out.print("\n\nThis is multiples of 15: \n");
+        int c = 0;
+        while (c < 1001) {
+            if (c % 15 == 0) {
+                System.out.print(c + " ");
+            }
+            c++;
         }
     }
 }
-
